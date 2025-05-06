963952e1111b221964c13de37ac562fe2136dcb8
duong duc thang <duongducthang142@gmail.com>
2025-05-06 11:01:06 +0700

    Upload HTML site

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let point=9.0;
+if(point>=8.0){
+    console.log("Học sinh giỏi");
+}
+point=7.0;
+
+if(point>=8.0){
+    console.log('Học sinh giỏi');
+}else{
+    console.log('Cần có gắng them')
+}
+
+if(point >=8.0){
+    console.log('Hoc sinh gioi');
+}else if(point <8.0 && point >=6.5){
+    console.log('Hoc sinh kha');
+}else  if(point >=5.0 && point <6.5){
+    console.log('Hoc sinh trung binh');
+}else{
+    console.log('Hoc sinh yeu');
+}
