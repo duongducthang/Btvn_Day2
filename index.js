917e82cec86e9e63ae8a8bde1e77ec72b4f8ec53
duong duc thang <duongducthang142@gmail.com>
2025-05-06 11:12:48 +0700

    add file

---
+++ index.js
@@ -0,0 +1,37 @@
+// console.log("Đang chạy...");
+// let a = 8;
+
+// if (a >= 8) {
+//     console.log("Học sinh giỏi");
+// } else {
+//     console.log("Học sinh khá");
+// }
+
+// console.log("Kết thúc chương trình");
+
+// let a=7;
+
+// if(a>=8){
+//     console.log("Hoc sinh gioi");
+// }else if(a>=6.5 && a<=7){
+//     console.log("Hoc sinh kha");
+// }else if(a>=4 && a<6.5){
+//     console.log("hoc sinh trung binh");
+// }else{
+//     console.log("hoc sinh yeu");
+// }
+
+switch(a){
+    case 8:
+    case 9:
+    case 10:
+        console.log("hoc sinh gioi");
+    case 7:
+        console.log("hoc sinh kha");
+    case 6:
+        console.log("hoc sinh trung binh");
+    case 5:
+        console.log("hoc sinh yeu");
+    default:
+        console.log("gia tri ko hop le");
+}
